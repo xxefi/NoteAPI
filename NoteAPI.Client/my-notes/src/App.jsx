2fe43bdc6700f9109ac1e820b186c993d4fb8eb9
Afgan <134388584+xxefi@users.noreply.github.com>
2024-07-15 04:01:54 +0400

    Update App.jsx

--- NoteAPI.Client/my-notes/src/App.jsx
+++ NoteAPI.Client/my-notes/src/App.jsx
@@ -51,7 +51,7 @@ export default function App() {
               fontWeight: "bold",
             }}
           >
-            Заметки пуст
+            No data found
           </p>
         )}
       </ul>
